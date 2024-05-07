d99e3b57e8e307ce9101fb8c3749ff6fc4210522
Mike Sanders <mikesndrs@gmail.com>
2024-05-07 23:54:18 +0200

    4D final

--- 4/D.cpp
+++ 4/D.cpp
@@ -10,8 +10,9 @@
 typedef long long ll;
 using namespace std;
 
-int handle_input(ll& w, ll& h, unordered_map<string, vector<ll>>& w_h_list) {
-  int n;
+int handle_input(ll& n, vector<vector<ll>>& w_h_list) {
+  ll w;
+  ll h;
   string s;
   string line;
   vector<ll> w_h_vec;
@@ -39,113 +40,61 @@ int handle_input(ll& w, ll& h, unordered_map<string, vector<ll>>& w_h_list) {
     key += s;
     w_h_vec.push_back(i + 1);
     if (w_h_vec[0] > w && w_h_vec[1] > h) {
-      w_h_list[key] = w_h_vec;
+      w_h_list.push_back(w_h_vec);
     }
   }
+  n = w_h_list.size();
   return 0;
 }
 
-int filter_w_h(vector<ll>& w_h, unordered_map<string, vector<ll>>& new_w_h) {
-  vector<string> key_list;
-  vector<ll> n_w_h;
-  string key;
-
-  for (const auto& pair : new_w_h) {
-    key = pair.first;
-    n_w_h = pair.second;
-    if (n_w_h[0] <= w_h[0] || n_w_h[1] <= w_h[1]) {
-      key_list.push_back(key);
-    }
-  }
-  for (string key : key_list) {
-    new_w_h.erase(key);
-  }
+int sort_w_h(vector<vector<ll>>& w_h_list) {
+  sort(w_h_list.begin(), w_h_list.end(), [](vector<ll> a, vector<ll> b) {return a[0] > b[0];});
   return 0;
 }
-int find_pareto_w_h(
-  unordered_map<string, vector<ll>>& w_h_list,
-  vector<string>& pareto_w_h,
-  string& key,
-  vector<ll>& w_h_i,
-  bool& should_add,
-  vector<string>& should_remove
+
+int get_dependency_list(
+  ll& n,
+  vector<vector<ll>>& w_h_list,
+  vector<ll>& max_chain_list,
+  vector<ll>& parent_list
 ) {
-  vector<ll> p_w_h_i;
-
-  for (const auto& pair : w_h_list) {
-    key = pair.first;
-    w_h_i = pair.second;
-    should_add = true;
-    should_remove = {};
-
-    for (const auto& p_key: pareto_w_h) {
-      p_w_h_i = w_h_list[p_key];
-      if (w_h_i[0] >= p_w_h_i[0] && w_h_i[1] >= p_w_h_i[1]) {
-        should_add = false;
-        break;
-      } else if (w_h_i[0] < p_w_h_i[0] && w_h_i[1] < p_w_h_i[1]) {
-        should_remove.push_back(p_key);
+  for (int i = 0 ; i < n; i++) {
+    for (int j = i; j < n; j++) {
+      if (
+        w_h_list[j][0] < w_h_list[i][0]
+        && w_h_list[j][1] < w_h_list[i][1]
+        && max_chain_list[j] <= max_chain_list[i]
+      ) {
+        max_chain_list[j] = max_chain_list[i] + 1;
+        parent_list[j] = i;
       }
     }
-    for (const auto& r_key: should_remove) {
-      pareto_w_h.erase(remove(pareto_w_h.begin(), pareto_w_h.end(), r_key), pareto_w_h.end());
-    }
-    if (should_add == true) {
-      pareto_w_h.push_back(key);
+  }
+  return 0;
+}
+
+int find_max_index(ll& j, vector<ll>& max_chain_list) {
+  j = max_element(max_chain_list.begin(), max_chain_list.end()) - max_chain_list.begin();
+  return 0;
+}
+
+int fill_best_chain_order(
+  ll& j,
+  vector<ll>& best_chain_order,
+  vector<vector<ll>>& w_h_list,
+  vector<ll>& parent_list
+) {
+  for (int i = 0 ; i < w_h_list.size(); i++) {
+    best_chain_order.push_back(w_h_list[j][2]);
+    j = parent_list[j];
+    if (j == -1) {
+      break;
     }
   }
   return 0;
 }
 
-int investigate_order(
-  vector<vector<ll>>& chain,
-  unordered_map<string, vector<ll>>& w_h_list,
-  ll& max_chain_length,
-  vector<int>& best_chain_order
-) {
-  int w_h_size = w_h_list.size();
-  int chain_size = chain.size();
-
-  if (w_h_size == 0) {
-    if (chain_size > max_chain_length) {
-      max_chain_length = chain_size;
-      best_chain_order = {};
-      for (vector<ll> chain_el : chain) {
-        best_chain_order.push_back(chain_el[2]);
-      }
-    }
-  } else if (chain_size + w_h_size > max_chain_length) {
-    vector<vector<ll>> new_chain;
-    vector<string> pareto_w_h;
-    unordered_map<string, vector<ll>> new_w_h;
-    vector<ll> w_h_i(3);
-    string key;
-    bool should_add;
-    vector<string> should_remove;
-
-    find_pareto_w_h(w_h_list, pareto_w_h, key, w_h_i, should_add, should_remove);
-    for (const auto& p_key : pareto_w_h) {
-      w_h_i = w_h_list[p_key];
-      new_chain = chain;
-      new_chain.push_back(w_h_i);
-      new_w_h = w_h_list;
-      filter_w_h(w_h_i, new_w_h);
-      investigate_order(new_chain, new_w_h, max_chain_length, best_chain_order);
-    }
-  }
-  return 0;
-}
-
-int beep(
-  vector<vector<ll>>& chain,
-  unordered_map<string, vector<ll>>& w_h_list,
-  ll& max_chain_length,
-  vector<int>& best_chain_order
-) {
-  return 0;
-}
-
-int print_result(ll max_chain_length, vector<int> best_chain_order) {
+int print_result(ll max_chain_length, vector<ll> best_chain_order) {
   if (max_chain_length > 0) {
     cout << max_chain_length << endl;
     for (ll chain_el : best_chain_order) {
@@ -159,31 +108,22 @@ int print_result(ll max_chain_length, vector<int> best_chain_order) {
 }
 
 int main() {
-  ll w = 0;
-  ll h = 0;
-  unordered_map<string, vector<ll>> w_h_list = {};
-  handle_input(w, h, w_h_list);
-
-  for (int i = 0; i < 200; i++) {
-    ll max_chain_length = 0;
-    vector<int> best_chain_order = {};
-    vector<vector<ll>> chain = {};
-    // investigate_order(chain, w_h_list, max_chain_length, best_chain_order);
-    beep(chain, w_h_list, max_chain_length, best_chain_order);
-    print_result(max_chain_length, best_chain_order);
+  ll n, w, h, j, max_chain_length;
+  vector<vector<ll>> w_h_list = {};
+  vector<ll> best_chain_order = {};
+  
+  handle_input(n, w_h_list);
+  vector<ll> parent_list(n, -1);
+  vector<ll> max_chain_list(n, 1);
+  sort_w_h(w_h_list);
+  get_dependency_list(n, w_h_list, max_chain_list, parent_list);
+  find_max_index(j, max_chain_list);
+  if (n > 0) {
+    max_chain_length = max_chain_list[j];
+  } else {
+    max_chain_length = 0;
   }
+  fill_best_chain_order(j, best_chain_order, w_h_list, parent_list);
+  print_result(max_chain_length, best_chain_order);
   return 0;
 }
-
-// 11 3 3
-// 1 1
-// 4 4
-// 5 6
-// 6 7
-// 8 9
-// 6 5
-// 7 6
-// 8 7
-// 9 8
-// 10 10
-// 3 3
